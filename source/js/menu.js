9b1029be3349b7e46e50a5b17e56b20b19f0a0cd
Adel <rizvanovaadel@gmail.com>
2020-05-06 15:04:25 +0300

    внесла правки, добавила слайдер

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,12 +1,10 @@
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
-navMain.classList.remove('main-nav--nojs');
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-navMain.classList.remove('main-nav--closed');
-navMain.classList.add('main-nav--opened');
-} else {
-navMain.classList.add('main-nav--closed');
-navMain.classList.remove('main-nav--opened');
-  }
-});
+const nav = document.querySelector(`.main-nav`);
+const toggle = nav.querySelector(`.main-nav__toggle`);
+
+nav.classList.remove(`main-nav--nojs`);
+
+function onToggleClick() {
+  nav.classList.toggle(`main-nav--opened`);
+}
+
+toggle.addEventListener(`click`, onToggleClick);
